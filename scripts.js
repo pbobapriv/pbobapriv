addf14f513d123846f8455d0883c8389331b6a68
pbobapriv <drrrrel@gmail.com>
2021-06-15 01:38:28 +0300

    progress

--- scripts.js
+++ scripts.js
@@ -7,7 +7,21 @@
 		fetch(url).then(res => res.json()).then(json => createPhotoPage(json));
 	}
 	function createPhotoPage(json){
-		console.log(json);
+		if (json.type === 'index') {
+			let html = '';
+			json.blocks.forEach(blockName => {
+				html += `<h2>${blockName}</h2>\n`;
+				html += `<a href="${blockName}.html" class="photo-line">\n`;
+				json[blockName].forEach(img => {
+					let space = img.indexOf(' ');
+					let src = space > -1 ? img.substr(0, space) : img;
+					let alt = space > -1 ? img.substr(space + 1) : img;
+					html += `<img src="${src}" alt="${alt}" title="${alt}">\n`;
+				});
+				html += '</a>\n';
+			});
+			document.querySelector('index-blocks');
+		}
 	}
 	
 })();
